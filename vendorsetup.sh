90cdb6a14b478c18f707825c1e1a8f93da294d93
Emirkan Acar <edorewiel@gmail.com>
2016-02-21 23:43:33 +0200

    Delete AOSP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,6 +23,3 @@
 add_lunch_combo cm_a500cg-userdebug
 add_lunch_combo cm_a500cg-user
 add_lunch_combo cm_a500cg-eng
-add_lunch_combo aosp_a500cg-userdebug
-add_lunch_combo aosp_a500cg-user
-add_lunch_combo aosp_a500cg-eng
